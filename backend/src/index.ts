6d99151fdb7cdc48ad9579ba6b31f15b616c0fb9
ozgur <lugrugzo@gmail.com>
2025-04-30 22:34:54 +0800

    chore: add express js and `/api/balance` endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,2 +1,16 @@
-type P = {};
-console.log("TS works?");
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/api/balance", (req, res) => {
+  const { address } = req.query;
+  console.log({ address });
+  res.json({ message: "Hello, world!" });
+});
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
